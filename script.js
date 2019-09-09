7b8985baaa2656e234f7743aa076d22495989a22
Josue <josue.n.rivera@outlook.com>
2019-09-09 01:17:17 -0400

    Display Nim Init
    The nim game can be displayed and the selected is printed.
    Two print are occuring at the moment

--- script.js
+++ script.js
@@ -1,94 +1,205 @@
-const app = new PIXI.Application({transparent: true, width: window.innerWidth, height: window.innerHeight});
+/*const app = new PIXI.Application({transparent: true, width: window.innerWidth, height: window.innerHeight});
 
-    document.body.appendChild(app.view);
+document.body.appendChild(app.view);
 
-    app.renderer.autoResize = true;
-    app.renderer.resize(window.innerWidth, window.innerHeight);
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
 
 
 
-    // create a texture from an image path
-    const texture = PIXI.Texture.from("assets/images/x.png");
+// create a texture from an image path
+const texture = PIXI.Texture.from("assets/images/x.png");
+
+
+// Scale mode for pixelation
+texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+
+for (let i = 0; i < 10; i++) {
+    createX(
+        Math.floor(Math.random() * 0.9 * app.screen.width),
+        Math.floor(Math.random() * 0.9 * app.screen.height),
+    );
+}
+
+function createX(x, y) {
+    // create our little bunny friend..
+    const bunny = new PIXI.Sprite(texture);
+
+    // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
+    bunny.interactive = true;
+
+    // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
+    bunny.buttonMode = true;
+
+    // center the bunny's anchor point
+    bunny.anchor.set(0.5);
+
+    // make it a bit bigger, so it's easier to grab
+    bunny.scale.set(0.2);
+
+    // setup events for mouse + touch using
+    // the pointer events
+    bunny
+        .on('pointerdown', onDragStart)
+        .on('pointerup', onDragEnd)
+        .on('pointerupoutside', onDragEnd)
+        .on('pointermove', onDragMove);
+
+    // For mouse-only events
+    // .on('mousedown', onDragStart)
+    // .on('mouseup', onDragEnd)
+    // .on('mouseupoutside', onDragEnd)
+    // .on('mousemove', onDragMove);
+
+    // For touch-only events
+    // .on('touchstart', onDragStart)
+    // .on('touchend', onDragEnd)
+    // .on('touchendoutside', onDragEnd)
+    // .on('touchmove', onDragMove);
+
+    // move the sprite to its designated position
+    bunny.x = x;
+    bunny.y = y;
+
+    // add it to the stage
+    app.stage.addChild(bunny);
+}
+
+function onDragStart(event) {
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    this.data = event.data;
+    this.alpha = 0.5;
+    this.dragging = true;
+}
+
+function onDragEnd() {
+    this.alpha = 1;
+    this.dragging = false;
+    // set the interaction data to null
+    this.data = null;
+}
+
+function onDragMove() {
+    if (this.dragging) {
+        const newPosition = this.data.getLocalPosition(this.parent);
+        this.x = newPosition.x;
+        this.y = newPosition.y;
+    }
+}
+
+function setup(){
     
+}*/
 
-    // Scale mode for pixelation
-    texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+const app = new PIXI.Application({transparent: true, width: window.innerWidth - 20, height: window.innerHeight - 20});
 
-    for (let i = 0; i < 10; i++) {
-        createX(
-            Math.floor(Math.random() * 0.9 * app.screen.width),
-            Math.floor(Math.random() * 0.9 * app.screen.height),
-        );
-    }
+document.body.appendChild(app.view);
 
-    function createX(x, y) {
-        // create our little bunny friend..
-        const bunny = new PIXI.Sprite(texture);
+PIXI.loader
+	.add("assets/images/x.png")
+	.load(setup);
 
-        // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
-        bunny.interactive = true;
+let dragging = [-1, -1];
+let level = -1;
 
-        // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
-        bunny.buttonMode = true;
+function setup() {
 
-        // center the bunny's anchor point
-        bunny.anchor.set(0.5);
+	let xs_cont = new PIXI.Container();
 
-        // make it a bit bigger, so it's easier to grab
-        bunny.scale.set(0.2);
+	let rows = createXs(xs_cont);
 
-        // setup events for mouse + touch using
-        // the pointer events
-        bunny
-            .on('pointerdown', onDragStart)
-            .on('pointerup', onDragEnd)
-            .on('pointerupoutside', onDragEnd)
-            .on('pointermove', onDragMove);
+	for (var i = 0; i < rows.length; i++) {
+		xs_cont.addChild(rows[i]);
+	}
 
-        // For mouse-only events
-        // .on('mousedown', onDragStart)
-        // .on('mouseup', onDragEnd)
-        // .on('mouseupoutside', onDragEnd)
-        // .on('mousemove', onDragMove);
+	xs_cont.position.x = (app.stage.width / 2) + (xs_cont.width / 2);
+	xs_cont.position.y = (app.stage.height / 2) + (xs_cont.width / 2);
 
-        // For touch-only events
-        // .on('touchstart', onDragStart)
-        // .on('touchend', onDragEnd)
-        // .on('touchendoutside', onDragEnd)
-        // .on('touchmove', onDragMove);
+	app.stage.addChild(xs_cont);
 
-        // move the sprite to its designated position
-        bunny.x = x;
-        bunny.y = y;
+}
 
-        // add it to the stage
-        app.stage.addChild(bunny);
-    }
+function createXs(cont) {
+	
+	let map = [3, 5, 7];
 
-    function onDragStart(event) {
-        // store a reference to the data
-        // the reason for this is because of multitouch
-        // we want to track the movement of this particular touch
-        this.data = event.data;
-        this.alpha = 0.5;
-        this.dragging = true;
-    }
+	let level_cont = [new PIXI.Container(),
+						new PIXI.Container(),
+						new PIXI.Container()];
 
-    function onDragEnd() {
-        this.alpha = 1;
-        this.dragging = false;
-        // set the interaction data to null
-        this.data = null;
-    }
+	//Add x to appropiate row
+	for (var i = 0; i < map.length; i++) {
+		let xs = createXRow(i, map[i], map[map.length - 1]);
 
-    function onDragMove() {
-        if (this.dragging) {
-            const newPosition = this.data.getLocalPosition(this.parent);
-            this.x = newPosition.x;
-            this.y = newPosition.y;
-        }
-    }
+		for (var j = 0; j < xs.length; j++) {
+			level_cont[i].addChild(xs[j]);
+		}
 
-    function setup(){
-        
-    }
+		//level_cont[i].position.x = (cont.width / 2) - (level_cont[i].width / 2);
+		level_cont[i].position.y = i * 100;
+	}
+
+	return level_cont;
+}
+
+function createXRow(level, n, l) {
+	let xs = [];
+
+	for (var i = 0; i < n; i++) {
+		let x_icon = createX((i + (l-n)/2) * 100, level);
+		x_icon.id = i;
+		x_icon.level = level;
+
+		xs.push(x_icon);
+	}
+
+	return xs;
+}
+
+function createX(x, y, texture) {
+
+	let x_texture = PIXI.loader.resources["assets/images/x.png"].texture;
+
+	const x_icon = new PIXI.Sprite(x_texture);
+
+    x_icon.interactive = true;
+
+    x_icon.buttonMode = true;
+
+    x_icon.anchor.set(0.5);
+
+    x_icon.scale.set(0.15);
+
+    x_icon
+        .on('pointerdown', onDragXStart)
+        .on('pointerup', onDragXEnd)
+        .on('pointerupoutside', onDragXEnd)
+        .on('pointermove', onDragXMove);
+
+    x_icon.x = x;
+    x_icon.y = y;
+
+	return x_icon;
+}
+
+function onDragXStart(event) {
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    this.data = event.data;
+    dragging[0] = this.id;
+}
+
+function onDragXEnd() {
+    // set the interaction data to null
+
+    dragging[1] = this.id;
+    this.data = null;
+    console.log(dragging);
+}
+
+function onDragXMove() {
+    
+}
